37a85e9dbdfc3175b7f73576edfceccdd6ec83f3
NathanDonagi <77026986+NathanDonagi@users.noreply.github.com>
2023-11-04 00:41:06 -0400

    2
    update speed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -69,7 +69,7 @@ public class RobotContainer {
       new ArmPIDCommand(arm, "coneMid"),
       new ClawCMD(claw),
       new WaitCommand(1.5),
-      new MoveAtSpeedForTime(swerveSubsystem, 10, 0, 0, 2)
+      new MoveAtSpeedForTime(swerveSubsystem, 1, 0, 0, 2)
     );
   }
 }
